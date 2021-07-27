6f4e9e4b286089fdf2cc2e64e35aa4e155dbc43b
Alp <alpjr.atilgan@gmail.com>
2021-07-27 15:48:44 -0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FootballComponent } from './football/football.component';
+import { AboutComponent } from './about/about.component';
+import { FormComponent } from './form/form.component';
+import { VideoComponent } from './video/video.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path:'football', component:FootballComponent},
+  {path:'about', component:AboutComponent},
+  {path:'form', component:FormComponent},
+  {path:'video', component:VideoComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FootballComponent } from './football/football.component';
+import { FormComponent } from './form/form.component';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FootballComponent,
+    FormComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
